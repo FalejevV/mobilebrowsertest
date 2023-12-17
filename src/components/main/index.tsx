2b43af752254b01727c02a9e4c388f02c0b67431
FalejevV <v.o.v.a@hotmail.com>
2023-12-17 13:38:17 +0200

    handmade swiping

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,50 +1,80 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import React from "react";
 import EmptyView from "../empty-view";
-import { PanInfo, motion, useDragControls } from "framer-motion";
 
 export default function ScrollExperience() {
-  const [currentPageIndex, setCurrentPageIndex] = useState(0);
-  const [viewSequence, setViewSequence] = useState([1, 2, 3, 4, 5, 6, 7]);
+	const dragRef = useRef(null);
+	const [currentPageIndex, setCurrentPageIndex] = useState(0);
+	const [viewSequence, setViewSequence] = useState([1, 2, 3, 4, 5, 6, 7]);
+	const [touchStart, setTouchStart] = useState(0);
+	function touchMoveHandler(e: React.TouchEvent<HTMLDivElement>) {
+		if (dragRef?.current) {
+			let swipeDifference = e.touches[0].screenY - touchStart;
+			if (swipeDifference > 100) {
+				swipeDifference = 100;
+			} else if (swipeDifference < -100) {
+				swipeDifference = -100;
+			}
 
-  function dragHandler(
-    event: MouseEvent | TouchEvent | PointerEvent,
-    info: PanInfo
-  ) {
-    event.preventDefault();
-    if (info.offset.y > 75 && currentPageIndex > 0) {
-      setCurrentPageIndex((prev) => prev - 1);
-    }
-    if (info.offset.y < -7 && currentPageIndex + 1 < viewSequence.length) {
-      setCurrentPageIndex((prev) => prev + 1);
-    }
-  }
-
-  return (
-    <div className="w-screen min-w-[100vw] h-dvh max-h-dvh no-scrollbar overflow-hidden">
-      <motion.div
-        drag="y"
-        dragConstraints={{ top: 0, bottom: 0 }}
-        dragSnapToOrigin
-        onDragEnd={dragHandler}
-        onDragStart={(e) => e.preventDefault()}
-        onDrag={(e) => e.preventDefault()}
-        style={{ touchAction: "none" }}
-      >
-        <div
-          style={{
-            transition: `transform 0.3s`,
-            transform: `translateY(-${currentPageIndex * 100}dvh)`,
-          }}
-          className={`min-w-[100vw] w-screen min-h-dvh pointer-events-none no-scrollbar`}
-        >
-          {viewSequence.map((item, index) => (
-            <EmptyView key={item} index={index} />
-          ))}
-        </div>
-      </motion.div>
-    </div>
-  );
+			(
+				dragRef.current as HTMLDivElement
+			).style.transform = `translateY(calc(${
+				currentPageIndex * 100 * -1
+			}dvh + ${swipeDifference * -1}px * -1))`;
+			(dragRef.current as HTMLDivElement).style.transition =
+				"transform 0s";
+		}
+	}
+	console.log(currentPageIndex);
+	function touchEndHandler(e: React.TouchEvent<HTMLDivElement>) {
+		let swipeDifference = e.changedTouches[0].screenY - touchStart;
+		if (swipeDifference > 80) {
+			setCurrentPageIndex((prev) => {
+				if (prev - 1 < 0) {
+					return prev;
+				} else {
+					return prev - 1;
+				}
+			});
+		} else if (swipeDifference < -80) {
+			setCurrentPageIndex((prev) => {
+				if (prev + 1 > viewSequence.length - 1) {
+					return prev;
+				} else {
+					return prev + 1;
+				}
+			});
+		}
+		if (dragRef?.current) {
+			(dragRef.current as HTMLDivElement).style.transition = "all 0.3s";
+			(dragRef.current as HTMLDivElement).style.transform = `translateY(${
+				currentPageIndex * 100 * -1
+			}dvh)`;
+		}
+	}
+	return (
+		<div className="w-screen min-w-[100vw] h-dvh max-h-dvh no-scrollbar overflow-hidden">
+			<div
+				className="w-screen min-w-full pointer-events-none no-scrollbar relative"
+				onTouchMove={touchMoveHandler}
+				onTouchStart={(e) => setTouchStart(e.touches[0].screenY)}
+				onTouchEnd={touchEndHandler}
+			>
+				<div
+					ref={dragRef}
+					style={{
+						transition: `transform 0.3s`,
+						transform: `translateY(-${currentPageIndex * 100}dvh)`,
+					}}
+					className={`min-w-[100vw] w-screen min-h-dvh pointer-events-none no-scrollbar relative`}
+				>
+					{viewSequence.map((item, index) => (
+						<EmptyView key={item} index={index} />
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
