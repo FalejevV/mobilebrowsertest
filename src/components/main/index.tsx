959a7c21f5af1c1a30c5cf521f31437e334368e6
FalejevV <v.o.v.a@hotmail.com>
2023-12-18 12:53:25 +0200

    fixed keyboard

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -7,50 +7,13 @@ import useDrag from "../use-drag-hook";
 import { useKeyboardNavigation } from "../fullscreen-navigation-hook";
 
 export default function ScrollExperience() {
-  const [viewSequence, setViewSequence] = useState([1, 2, 3, 4, 5, 6, 7]);
-  const {
-    dragRef,
-    touchMoveHandler,
-    touchEndHandler,
-    touchStart,
-    setTouchStart,
-    currentPageIndex,
-    setCurrentPageIndex,
-    touchStartHandler,
-  } = useDrag({
-    length: viewSequence.length,
-    triggerDistance: 80,
-    stickDistance: 35,
-    maxDistance: 100,
-  });
+	const [viewSequence, setViewSequence] = useState([1, 2, 3, 4, 5, 6, 7]);
 
-  useKeyboardNavigation({
-    setCurrentPageIndex,
-    currentPageIndex,
-    length: viewSequence.length,
-  });
-  return (
-    <div className="w-screen min-w-[100vw] h-dvh max-h-dvh no-scrollbar overflow-hidden">
-      <div
-        className="w-screen min-w-full pointer-events-none no-scrollbar relative"
-        onTouchMove={touchMoveHandler}
-        onTouchStart={touchStartHandler}
-        onTouchEnd={touchEndHandler}
-      >
-        <div
-          id="mainScroll"
-          ref={dragRef}
-          style={{
-            transition: `transform 0.3s`,
-            transform: `translateY(-${currentPageIndex * 100}dvh)`,
-          }}
-          className={`min-w-[100vw] w-screen min-h-dvh pointer-events-none no-scrollbar relative`}
-        >
-          {viewSequence.map((item, index) => (
-            <EmptyView key={item} index={index} />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="w-screen min-w-[100vw] h-dvh no-scrollbar overflow-y-auto overflow-x-hidden snap-always snap-y snap-center snap-mandatory">
+			{viewSequence.map((item, index) => (
+				<EmptyView key={item} index={index} />
+			))}
+		</div>
+	);
 }
